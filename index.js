ab13738bee2e2a562ac66128f72bb4333572a55a
Arpit Mathur <arpitmathur23@gmail.com>
2023-07-04 04:13:55 +0530

    added APIs

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express=require("express");
 const app=express()
+const redis=require("redis")
 
+const redisClient = redis.createClient(6379,'127.0.0.1');
 
+redisClient.connect()
+redisClient.on("connect", (err)=>{
+    if(err){
+        console.log(err)
+    }
+    else console.log("connected to redis-server")
+} );
 
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.get("/set",(req,res)=>{
+    res.send("set page")
+})
+
+app.post("/set",async(req,res)=>{
+    let key= await req.body.key
+    let value= await req.body.value
+    await redisClient.set(key,value)
+    res.send("done")
+})
+
+app.get("/get",async(req,res)=>{
+    // const key=req.body.key
+    const response=await redisClient.get("name")
+    res.json(response);
+})
+
+app.listen(5000, () =>{
+  console.log("Server listening on http://localhost:5000");
 });
